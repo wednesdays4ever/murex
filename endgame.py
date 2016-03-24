5bdedbfcbed5686ee8502b8b6db4ef97374767e2
Robert Norman <rznorman@gmail.com>
2016-03-23 23:20:39 -0400

    whole pyramid entering sequence

---
+++ endgame.py
@@ -0,0 +1,188 @@
+def pyramid_approach():
+	print """
+	It felt like a small island when you arrived,
+	but as you take your first decisive steps toward
+	this shining monument,
+	you wonder how you could ever have associated
+	small
+	with this pillar of wonder.
+
+	You take slow, straight steps toward the shell.
+	It is the Murex now,
+	no longer just the shining spire.
+	You know because it whispers to you.
+	
+	You must get closer,
+	there is some great knowledge there,
+	inside the pyramid,
+	that you must encounter.
+
+	As you approach,
+	a warm sensation,
+	hot even,
+	flows out over your skin,
+	as if your blood was boiling over 
+	and spilling out through your pores.
+	It is an intense and unbearable feeling,
+	that you cannot imagine bearing for long.
+
+	Your concentration is broken with a sharp slap.
+
+	You look down for the first time in 
+	what might have been an hour
+	to see a tiny, sharp-faced man
+	in a torn and filthy purple robe.
+	YOU'RE LATE
+	the weasel croaks.
+
+	How do you respond to this interruption?
+	"""
+def dobbins_attack():
+	print """ Whoever this small and insignificant creature was
+	has crumpled beneath a newly woken might.
+	You feel strong as you step over the creature.
+	As your eyes return to the Murex,
+	it seems to adopt a faint lavender glow.
+
+	The sun shines a faint orange 
+	as it sets
+	that pales next to the glowing spire
+
+	A stone door lies ajar in front of you.
+	Do you enter this beyond?
+	"""
+
+def dobbins_question():
+	print """ 
+	I AM HIS HUMAN EXCELLENCY THE HIGH PRIEST'S
+	MOST TRUSTED SERVANT
+	AND THE ONE WHO HAS BORNE THE BRUNT OF
+	HIS FRUSTRATION AND IMPATIENCE
+	AT YOUR DELAYED ARRIVAL.
+	AT THE SIGHT OF YOU I FEEL BOTH RELIEF
+	AND THE DESIRE TO STRIKE YOU ABOUT THE FACE
+	AND BACK
+	AND ANKLES
+	AND BUTTOCKS
+	AS I HAVE BEEN STRUCK
+	ABOUT THE FACE AND BACK AND ANKLES AND BUTTOCKS.
+	BUT I WILL RESIST THE URGE
+	YOUR BLOOD IS NOT MINE TO SPILL.
+	MY MASTER AND THE ONE WHOM WE BOTH SERVE
+	AWAIT.
+	"""
+	#if violence, go to attack
+	#if other, he forces you forward
+
+def dobbins_taymor():
+	print """ 
+	THE HIGH PRIEST AWAITS YOU.
+	YOU ARE LATE,
+	AND IT IS MY PERSONAL WISH TO PUNISH YOU FOR IT,
+	BUT YOUR TARDINESS MAY BE EXCUSED BY OUR MASTER,
+	IF YOU HURRY NOW TO BEND THE KNEE.
+	"""
+	#IF violence go to attack
+	#if something about approach, do that
+	#if other, the hurry forward
+
+def dobbins_other():
+	print """ 
+	This insolent man grabs your shoulders,
+	and shoves you along,
+	yelping 
+	YOU ARE LATE,
+	PROCEED TO THE RITUAL!
+
+	You feel somewhere deep inside you,
+	the knowledge that you are able
+	to crush this fragile idiot,
+	and the desire to do so.
+
+	But the call of the Murex is stronger
+	than your thirst for violence,
+	and you follow his push without causing him harm.
+
+	A stone door stands before you,
+	ajar."""
+
+def dobbins_hurry_forward():
+	print """ 
+	GET GOING!
+	THE COMING IS COMING
+	AND YOU MUSTN'T MISS IT!
+
+	The man is nervous and simple.
+	He senses the approaching greatness,
+	but can articulate none of its glory.
+	You feel a mix of pity and disgust.
+	This creature will not reach a higher consciousness.
+
+	You step past the ant without crushing it,
+	towards a stone door to the pyramid
+	that stands ajar,
+	welcoming you home.
+	""" 
+
+def pyramid_approach():
+	print """ 
+	You step past the dirty fool
+	towards destiny.
+
+	A large stone door to the pyramid
+	stands before you,
+	ajar.
+	"""
+
+def pyramid_enter():
+	print """ 
+	A skylight at the peak of the pyramid
+	exposes the inner labyrinth of the Murex,
+	twisting upwards,
+	beyond comprehension,
+	and flooding the ritual chamber with a lavender light.
+
+	Taymor, no older than you remember him,
+	stands directly under the source of light,
+	surrounded by the mummified remains of eleven men,
+	suspended from the ceiling by silver chains.
+
+	He smiles at you with these bright eyes
+	that remind you of a photo you remember from
+	a textbook in elementary school,
+	of a young woman who has just seen her fiancee
+	returning from war, 
+	one of the old ones.
+	And her face is so full 
+	of confidence and relief and disbelief and love and hope.
+	It is right next to elation but also on the edge of tears.
+	It was a beautiful expression,
+	and you are surpised to see it mirrored now.
+
+	MY BLOOD,
+	MY RELATION.
+	HERE YOU ARE,
+	AND JUST IN TIME TO WITNESS THE NEW WORLD.
+	WON'T YOU COME AND HUG YOUR GREAT-GRANDFATHER?
+
+	"""
+
+def pyramid_other():
+	pass
+	#the majesty is something that has perhaps been forever unknown to this world,
+	#you are right to bask in it 
+
+def dobbins_encounter():
+	pass
+
+def pyramid_survey():
+	pass
+
+def taymor_speech():
+	pass
+
+def ritual_begin():
+	pass
+
+def ritual_interrupt():
+	pass
